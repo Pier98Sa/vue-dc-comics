8c070cde14fa41e31f016246ea9e169068c0bd7e
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-02-21 20:58:43 +0100

    Footer top completato

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -1,7 +1,68 @@
 <template>
-  <div>
+  <footer>
+    <div class="footer-top container">
+      <div class="footer-menu">
+        <div class="col sx">
+          <h2>Dc Comics</h2>
+        <ul>
+          <li><a href="#">Characters</a></li>
+          <li><a href="#">Comics</a></li>
+          <li><a href="#">Movies</a></li>
+          <li><a href="#">TV</a></li>
+          <li><a href="#">Games</a></li>
+          <li><a href="#">Videos</a></li>
+          <li><a href="#">News</a></li>
+        </ul>
 
-  </div>
+        <h2>Shop</h2>
+        <ul>
+          <li><a href="#">Shop DC</a></li>
+          <li><a href="#">Shop DC Collectibles</a></li>
+        </ul>
+        </div>
+        <div class="col center">
+          <h2>DC</h2>
+        <ul>
+          <li><a href="#">Terms of Use</a></li>
+          <li><a href="#">Privacy policy (New)</a></li>
+          <li><a href="#">Ad Choices</a></li>
+          <li><a href="#">Advertising</a></li>
+          <li><a href="#">Jobs</a></li>
+          <li><a href="#">Subscriptions</a></li>
+          <li><a href="#">Talent Workshop</a></li>
+          <li><a href="#">CPSC Certificates</a></li>
+          <li><a href="#">Ratings</a></li>
+          <li><a href="#">Shop Help</a></li>
+          <li><a href="#">Contact US</a></li>
+        </ul>
+        </div>
+
+        <div class="col dx">
+          <h2>Sites</h2>
+          <ul>
+            <li><a href="#">DC</a></li>
+            <li><a href="#">MAD Magazine</a></li>
+            <li><a href="#">DC Kids</a></li>
+            <li><a href="#">DC Universe</a></li>
+            <li><a href="#">DC Power Visa</a></li>
+          </ul>
+        </div>
+        
+
+        
+      </div>
+
+      <div class="logo">
+        <img src="../assets/img/dc-logo-bg.png" alt="dc logo">
+      </div>
+
+    </div>
+
+    <div class="footer-bottom ">
+
+    </div>
+
+  </footer>
 </template>
 
 <script>
@@ -13,4 +74,58 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 @import "../assets/style/variables.scss";
+
+footer{
+  background-image: url(../assets/img/footer-bg.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 500px;
+
+  .footer-top{
+    height: 350px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    overflow: hidden;
+
+    .footer-menu{
+      display: flex;
+      
+
+      .col{
+        padding: 20px;    
+
+        h2{
+          color: $tertiaryColor;
+          font-size: 20px;
+          text-transform: uppercase;
+          padding: 10px 0 10px 0;
+        }
+
+        li{
+          list-style: none;
+
+          a{
+            text-decoration: none;
+            color: $quaternaryColor;
+            font-size: 12px;
+            padding: 10px 0 10px 0;
+          }
+        }
+
+      }
+    }
+
+    .logo{
+      
+    }
+  }
+
+  .footer-bottom{
+    height: 150px;
+    background-color: $primaryColor;
+  }
+}
+
+
 </style>
--- src/components/MyProduct.vue
+++ src/components/MyProduct.vue
@@ -1,16 +1,106 @@
 <template>
-  <div>
+  <div class="product">
 
+    <div class="container product-list" >
+
+        <div class="card" v-for="(link,index) in links" :key="index">
+          <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+          <!--<img :src="`../assets/img/${link.img}`" alt="">-->
+          <a :href="link.url">{{link.text}}</a>
+        </div>
+
+        <!--<div class="card">
+          <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+          <a href="#">Digital Comics</a>
+        </div>
+
+        <div class="card">
+          <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+          <a href="#">Digital Comics</a>
+        </div>
+
+        <div class="card">
+          <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+          <a href="#">Digital Comics</a>
+        </div>
+
+        <div class="card">
+          <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+          <a href="#">Digital Comics</a>
+        </div> -->
+
+    </div>
+ 
   </div>
 </template>
 
 <script>
 export default {
   name: 'MyProduct',
+  data() {
+    return {
+      links: [
+        {
+          text: "Digital Comics",
+          url: "#",
+          img: "buy-comics-digital-comics.png"
+        },
+        {
+          text: "dc merchandise",
+          url: "#",
+          img: "buy-comics-merchandise.png",
+        },
+        {
+          text: "subscription",
+          url: "#",
+          img: "buy-comics-shop-locator.png",
+        },
+        {
+          text: "comic shop locator",
+          url: "#",
+          img: "buy-comics-subscriptions.png",
+        },
+        {
+          text: "dc power visa",
+          url: "#",
+          img: "buy-dc-power-visa.svg",
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 @import "../assets/style/variables.scss";
+
+.product{
+  background-color: $secondaryColor;
+  height: 200px;
+}
+
+img{
+  width: 50px;
+}
+
+.product-list{
+  display: flex;
+  height: 100%;
+  justify-content: space-around;
+  align-items: center;
+
+  .card{
+    display: flex;
+    align-items: center;
+
+    a{
+      margin-left: 10px;
+      text-decoration: none;
+      text-transform: uppercase;
+      color: $tertiaryColor;
+    }
+  }
+
+}
 </style>
