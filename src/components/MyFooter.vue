ef184177290fa460c42f6c99de044f6fe807dfdf
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-02-22 16:03:36 +0100

    Myfooter completato

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -2,42 +2,48 @@
   <footer>
     <div class="footer-top container">
       <div class="footer-menu">
-        <div class="col sx">
+        
+        <div class="col">
           <h2>Dc Comics</h2>
-        <ul>
-          <li><a href="#">Characters</a></li>
-          <li><a href="#">Comics</a></li>
-          <li><a href="#">Movies</a></li>
-          <li><a href="#">TV</a></li>
-          <li><a href="#">Games</a></li>
-          <li><a href="#">Videos</a></li>
-          <li><a href="#">News</a></li>
-        </ul>
-
-        <h2>Shop</h2>
-        <ul>
-          <li><a href="#">Shop DC</a></li>
-          <li><a href="#">Shop DC Collectibles</a></li>
-        </ul>
+          <ul>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">News</a></li>
+          </ul>
         </div>
-        <div class="col center">
+        
+        <div class="col">
+          <h2>Shop</h2>
+          <ul>
+            <li><a href="#">Shop DC</a></li>
+            <li><a href="#">Shop DC Collectibles</a></li>
+          </ul>
+        </div>
+        
+        
+        <div class="col">
           <h2>DC</h2>
-        <ul>
-          <li><a href="#">Terms of Use</a></li>
-          <li><a href="#">Privacy policy (New)</a></li>
-          <li><a href="#">Ad Choices</a></li>
-          <li><a href="#">Advertising</a></li>
-          <li><a href="#">Jobs</a></li>
-          <li><a href="#">Subscriptions</a></li>
-          <li><a href="#">Talent Workshop</a></li>
-          <li><a href="#">CPSC Certificates</a></li>
-          <li><a href="#">Ratings</a></li>
-          <li><a href="#">Shop Help</a></li>
-          <li><a href="#">Contact US</a></li>
-        </ul>
+          <ul>
+            <li><a href="#">Terms of Use</a></li>
+            <li><a href="#">Privacy policy (New)</a></li>
+            <li><a href="#">Ad Choices</a></li>
+            <li><a href="#">Advertising</a></li>
+            <li><a href="#">Jobs</a></li>
+            <li><a href="#">Subscriptions</a></li>
+            <li><a href="#">Talent Workshop</a></li>
+            <li><a href="#">CPSC Certificates</a></li>
+            <li><a href="#">Ratings</a></li>
+            <li><a href="#">Shop Help</a></li>
+            <li><a href="#">Contact US</a></li>
+          </ul>
         </div>
+       
 
-        <div class="col dx">
+        <div class="col">
           <h2>Sites</h2>
           <ul>
             <li><a href="#">DC</a></li>
@@ -46,7 +52,9 @@
             <li><a href="#">DC Universe</a></li>
             <li><a href="#">DC Power Visa</a></li>
           </ul>
+  
         </div>
+        
         
 
         
@@ -106,19 +114,21 @@ footer{
     overflow: hidden;
 
     .footer-menu{
-      display: flex;
-      
+      height: 100%;
+      display: flex; 
+      flex-direction: column;
+      flex-wrap: wrap;
 
       .col{
-        padding: 20px;    
-
+        padding: 10px;
+        
         h2{
           color: $tertiaryColor;
           font-size: 20px;
           text-transform: uppercase;
           padding: 10px 0 10px 0;
         }
-
+       
         li{
           list-style: none;
 
@@ -128,9 +138,12 @@ footer{
             font-size: 12px;
             padding: 10px 0 10px 0;
           }
-        }
 
+        }
       }
+
+      
+
     }
 
   }
